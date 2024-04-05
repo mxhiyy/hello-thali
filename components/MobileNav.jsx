5f605a8a2e6a9e19bb8b2695abdc422372c79629
Eshika rawat <eshikarawat02@gmail.com>
2024-04-05 22:27:41 +0530

    done with navbar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -15,45 +15,56 @@ import { usePathname } from "next/navigation";
 const MobileNav = () => {
   const pathname = usePathname();
   return (
-    <section className="w-full max-w-[264px]">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Image
-            src="/icons/hamburger.svg"
-            alt="menu-icon"
-            width={36}
-            height={36}
-            className="cursor-pointer sm:hidden"
-          />
-        </SheetTrigger>
-        <SheetContent side={"left"} className="bg-green-1 border-none">
-          <div className="h-full flex flex-col justify-between items-center overflow-y-auto">
-            <SheetClose asChild>
-              <section className="flex h-full flex-col gap-8 pt-16 text-slate">
-                {NavbarLink.map((link) => {
-                  const isActive = pathname === link.route;
+    <main className="w-[95vw] flex justify-between ">
+      <Link href="/">
+        <Image
+          src="/icons/hellothali-logo.png"
+          alt="logo"
+          width={40}
+          height={40}
+          className="sm:hidden"
+        />
+      </Link>
+      <section className="w-1/2 flex justify-end">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Image
+              src="/icons/hamburger.svg"
+              alt="menu-icon"
+              width={36}
+              height={36}
+              className="cursor-pointer sm:hidden"
+            />
+          </SheetTrigger>
+          <SheetContent side={"left"} className="bg-green-1 border-none">
+            <div className="h-full flex flex-col justify-between items-center overflow-y-auto">
+              <SheetClose asChild>
+                <section className="flex h-full flex-col gap-8 pt-16 text-slate">
+                  {NavbarLink.map((link) => {
+                    const isActive = pathname === link.route;
 
-                  return (
-                    <SheetClose asChild key={link.route}>
-                      <Link
-                        href={link.route}
-                        key={link.label}
-                        className={cn(
-                          "flex gap-8 items-center w-full max-w-60 text-3xl font-semibold hover:text-white",
-                          { "text-white": isActive }
-                        )}
-                      >
-                        {link.label}
-                      </Link>
-                    </SheetClose>
-                  );
-                })}
-              </section>
-            </SheetClose>
-          </div>
-        </SheetContent>
-      </Sheet>
-    </section>
+                    return (
+                      <SheetClose asChild key={link.route}>
+                        <Link
+                          href={link.route}
+                          key={link.label}
+                          className={cn(
+                            "flex gap-8 items-center w-full max-w-60 text-3xl font-semibold hover:text-white",
+                            { "text-white": isActive }
+                          )}
+                        >
+                          {link.label}
+                        </Link>
+                      </SheetClose>
+                    );
+                  })}
+                </section>
+              </SheetClose>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </section>
+    </main>
   );
 };
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,21 +9,34 @@ import { NavbarLink } from "@/constants";
 import MobileNav from "./MobileNav";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+
 
 const Navbar = () => {
   const pathname = usePathname();
+
   return (
     <nav className="bg-green-1 p-3 w-screen">
       <div className="lg:w-4/5 m-auto hidden sm:flex items-center justify-between">
-        <div className="sm:w-[350px] lg:w-[50%] xl:w-2/5 flex justify-end">
+        <div className="sm:w-[350px] lg:w-[50%] xl:w-2/5 flex justify-between items-center">
           {/* hello thali logo */}
+          <Link href="/">
+          <Image
+            src="/icons/hellothali-logo.png"
+            alt="logo"
+            width={40}
+            height={40}
+          />
+          </Link>
           <div className=" sm:w-[300px] lg:w-3/4 flex justify-between">
             {NavbarLink.map((link) => {
               const isActive = pathname === link.route;
               return (
                 <Link
                   key={link.label}
-                  className={cn("text-slate cursor-pointer hover:text-white", {"text-white" : isActive})}
+                  className={cn("text-slate cursor-pointer hover:text-white", {
+                    "text-white": isActive,
+                  })}
                   href={link.route}
                 >
                   {link.label}
